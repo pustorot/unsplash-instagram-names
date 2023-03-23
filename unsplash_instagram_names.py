381a735f9bc32b924eb945ca053e325287804e19
pustorot <127698994+pustorot@users.noreply.github.com>
2023-03-23 04:46:30 +0300

    Update unsplash_instagram_names.py

--- unsplash_instagram_names.py
+++ unsplash_instagram_names.py
@@ -1,16 +1,21 @@
 # Import the requests library to make HTTP requests
 import requests
+import pandas as pd
 
 # Define the API endpoint for searching for users on Unsplash
 endpoint = 'https://api.unsplash.com/search/users'
 
 # Set the value for the first name to filter the results
-first_name = 'Alice'
+#first_name = 'Emma'
+location = 'Istanbul'
 
 # Set up the query parameters as a dictionary with the first_name parameter
 # and your Unsplash API access key as the value for the Authorization header
 params = {
-    'query': first_name
+    #'query': first_name,
+    'query': location,
+    'instagram_username': 'True',
+    'per_page': 100,
 }
 headers = {
     'Authorization': 'Client-ID <your_access_key>'
@@ -25,9 +30,12 @@ if response.status_code == 200:
     data = response.json()
     # Extract the 'results' key from the JSON data, which contains a list of user objects
     results = data['results']
-    # Loop through the user objects and print out the 'instagram_username' and 'username' fields
+    # Loop through the user objects and print out the 'instagram_username' and other accessible fields
     for user in results:
-        print(user['instagram_username'], user['username'])
+        if user["instagram_username"] is not None:
+            print(user['instagram_username'], user['username'], user['first_name'], user['last_name'], user['total_likes'],
+                 user['total_photos'], user['location'], user['twitter_username'])
+
 else:
     # If the request was not successful, print out an error message
     print('Error: Request failed with status code', response.status_code)
